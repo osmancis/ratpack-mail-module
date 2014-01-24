14af38ca70e5d9eaca20e86909af6d2c1f0f6590
Stefano Gualdi <stefano.gualdi@gmail.com>
2014-01-24 11:27:29 +0100

    Fix checkstyle violations

--- src/main/java/ratpack/modules/mail/internal/DefaultMailConfig.java
+++ src/main/java/ratpack/modules/mail/internal/DefaultMailConfig.java
@@ -1,19 +1,38 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.modules.mail.internal;
 
 import com.google.common.base.Objects;
 import ratpack.modules.mail.MailConfig;
 
+/**
+ * @author Stefano Gualdi <stefano.gualdi@gmail.com>
+ */
 public class DefaultMailConfig implements MailConfig {
 
-  private String host = null;
+  private String host;
   private int port = 25;
   private String protocol = "smtp";
-  private boolean auth = false;
-  private String username = null;
-  private String password = null;
-  private boolean starttls = false;
+  private boolean auth;
+  private String username;
+  private String password;
+  private boolean starttls;
   private String channel = "plain";
-  private boolean debug = false;
+  private boolean debug;
 
   public String getHost() {
     return host;
